96cd6a7a67513f4fb20b5886325e9852582481e7
sun <hsunvirus@gmail.com>
2025-07-08 06:23:48 +0900

    Refactor: UI 개선

--- src/pages/chat/ChatPage.jsx
+++ src/pages/chat/ChatPage.jsx
@@ -555,9 +555,7 @@ const Chat = () => {
                 <div className="chat-container">
                     <div className="chat-header">
                         💬  <span id="currentRoomName">{roomName}</span>
-                        <span className={`connection-status ${stompClientRef.current?.connected ? 'connected' : 'disconnected'}`}>
-                            {connectionStatus}
-                        </span>
+                        <span className={`connection-status ${stompClientRef.current?.connected ? 'connected' : 'disconnected'}`}></span>
                         <button id="openMembersBtn" title="채팅 멤버 보기" onClick={toggleOverlay}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28">
                                 <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3 1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5C15 14.17 10.33 13 8 13zm8 0c-.29 0-.62.02-.97.05C17.16 14.1 19 15.03 19 16.5V19h5v-2.5c0-2.33-4.67-3.5-7-3.5z" />
